a9b304419207859af3bc1ad9264f5b83f5746489
Omar Qasirov <omarbmohamad@gmail.com>
2022-05-28 08:54:41 -0500

    New driver class and shape class

--- typescript-training/05-inheritance/ArrayDriver.js
+++ typescript-training/05-inheritance/ArrayDriver.js
@@ -4,8 +4,13 @@ var Shape_1 = require("./Shape");
 var Circle_1 = require("./Circle");
 var Rectangle_1 = require("./Rectangle");
 var myShape = new Shape_1.Shape(10, 15);
-console.log(myShape.getInfo());
 var myCircle = new Circle_1.Circle(5, 10, 20);
-console.log(myCircle.getInfo());
 var myRectangle = new Rectangle_1.Rectangle(0, 0, 3, 7);
-console.log(myRectangle.getInfo());
+var theShapes = [];
+theShapes.push(myShape);
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+for (var _i = 0, theShapes_1 = theShapes; _i < theShapes_1.length; _i++) {
+    var tempShape = theShapes_1[_i];
+    console.log(tempShape.getInfo());
+}
--- typescript-training/05-inheritance/ArrayDriver.ts
+++ typescript-training/05-inheritance/ArrayDriver.ts
@@ -4,10 +4,15 @@ import { Rectangle } from "./Rectangle";
 
 
 let myShape = new Shape(10,15);
-console.log(myShape.getInfo());
-
 let myCircle = new Circle(5,10,20);
-console.log(myCircle.getInfo());
-
 let myRectangle = new Rectangle(0,0,3,7);
-console.log(myRectangle.getInfo());
+
+let theShapes : Shape[] = [];
+
+theShapes.push(myShape);
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+
+for (let tempShape of theShapes){
+    console.log(tempShape.getInfo());
+}
